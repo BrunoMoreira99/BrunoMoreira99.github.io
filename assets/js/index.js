b2291d4718490eb79df837145f12fa846ec1f0f4
Bruno Moreira <brunomcmoreira99@gmail.com>
2019-01-07 06:30:49 +0000

    Add dark theme Update a few things

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,4 +27,43 @@ window.addEventListener('load', () => {
 
     // Enable all tooltips.
     $('[data-toggle="tooltip"]').tooltip();
+    
+    // Handle dark mode toggle
+    $('.dark-mode-toggler .nav-link').click(() => {
+        if ($('body').hasClass('t--light')) {
+            $('body').addClass('t--dark').removeClass('t--light');
+            setCookie('_theme', 't--dark');
+        } else {
+            $('body').addClass('t--light').removeClass('t--dark');
+            setCookie('_theme', 't--light');
+        }
+    });
+    
+    const theme = getCookie('_theme');
+    if (theme) $('body').addClass(theme);
 });
+
+function setCookie(name, value, days) {
+    let expires = "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
